e232cbf8dd67a1b45e11112e170f587bb3a12796
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-23 15:11:28 -0300

    criando o Cabeçalho

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,9 +1,19 @@
 import { styled } from "styled-components"
 
-const HeaderEstilizado = styled
+const HeaderEstilizado = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    img {
+        max-width: 212px;
+        
+    }
+`
 
 const Cabecalho = () => {
-    return ( <>
-    
-    </>)
+    return ( <HeaderEstilizado>
+    <img src="/imagens/logo.png"  alt=""/>
+    </HeaderEstilizado>)
 }
+
+export default Cabecalho
