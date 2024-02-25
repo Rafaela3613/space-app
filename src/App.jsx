ef5fb9fd82ed95d499624db9e5400fea8254bce4
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-25 16:28:42 -0300

    Importando as imagens

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import Banner from "./componentes/Banner"
 import bannerBackground from './assets/banner.png'
 import Galeria from "./componentes/Galeria"
 
+import fotos from './fotos.json'
+import { useState } from "react"
+
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
 `
+
 const AppContainer = styled.div`
   width: 1440px;
   margin: 0 auto;
@@ -19,7 +23,7 @@ const AppContainer = styled.div`
 
 const MainContainer = styled.main`
   display: flex;
-  gap:  24px;
+  gap: 24px;
 `
 
 const ConteudoGaleria = styled.section`
@@ -28,26 +32,24 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `
 
-function App() {
-
-
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
   return (
     <FundoGradiente>
       <EstilosGlobais />
       <AppContainer>
-          <Cabecalho />
-          <MainContainer>
+        <Cabecalho />
+        <MainContainer>
           <BarraLateral />
           <ConteudoGaleria>
-          <Banner
-          texto="A galeria mais completa de fotos do espaço!"
-          backgroundImage={bannerBackground}
-          />
-          <Galeria />
+            <Banner
+              texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={bannerBackground}
+            />
+            <Galeria fotos={fotosDaGaleria}/>
           </ConteudoGaleria>
-          </MainContainer>
+        </MainContainer>
       </AppContainer>
-
     </FundoGradiente>
   )
 }
